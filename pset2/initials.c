ad0ef09ef4a81143c1b03aa55b33926285f872df
Amr Elghobary <amr.elghobary@gmail.com>
2016-08-03 21:11:09 +0200

    pset_02: initials.c

---
+++ pset2/initials.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <cs50.h>
+#include <string.h>
+
+
+int main(void)
+{
+
+    string name = GetString();
+
+    for (int i = 0, n = strlen(name); i < n - 1; i++)
+        if ( name[i] == ' ')
+            printf("%c", toupper(name[i + 1]));
+        else if (i == 0)
+            printf("%c", name[i]);
+
+    printf("\n");
+    return 0;
+}
